a8b0e1f1fe11515a1c510832dcabe89ccf38a250
Laitila93 <Laitila93@gmail.com>
2025-04-08 13:03:13 +0200

    uiou

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts">
 
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+const msg = "il"
 </script>
 
 <template>
-
+      <HelloWorld :msg= "msg" />
 </template>
