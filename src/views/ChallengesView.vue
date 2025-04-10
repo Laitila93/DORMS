696704f8e39c4c8a4cc5821aa3fa2fe725a4492c
EliasNylund <nylund.elias1@gmail.com>
2025-04-10 14:37:02 +0200

    Description of ChallengeView and StatsView

---
+++ src/views/ChallengesView.vue
@@ -0,0 +1,5 @@
+<!--View where user can see all daily, weekly, monthly, and special
+     challenges. Current, completed, and future challenges can be
+     viewed.
+    Total collected points and a direct link to the shop is available.
+    There is also a function for the user to change daily challenges.-->
---
+++ src/views/StatsView.vue
@@ -0,0 +1,3 @@
+<!--View where user can see more in-depth statisics of water consumption.
+    Both pure data and graphs are available, and can be sorted by time.
+    Can go in to "fullscreen view" of each graph etc.-->
