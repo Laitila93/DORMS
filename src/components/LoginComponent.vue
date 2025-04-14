b892af329c54c8cde4216807e81083cbbe2fe3c0
Emanru <emil.anbacken.rudfeldt@gmail.com>
2025-04-14 18:44:39 +0200

    add LoginComponent

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex flex-col space-y-4">
+    <input
+      type="text"
+      :id= "usernameID"
+      class="border border-gray-300 rounded p-2 w-full"
+      :placeholder="usernamePlaceholder"
+      v-model ="username"
+      @keyup.enter="redirectToTank"
+    />
+
+    <input
+      type="text"
+      :id = "passwordID"
+      class="border border-gray-300 rounded p-2 w-full"
+      :placeholder="passwordPlaceholder"
+      v-model = "password"
+      @keyup.enter="redirectToTank"
+      />
+
+    <router-link
+      to="/tank/" 
+      class="bg-amber-500 hover:bg-amber-600 
+      transition-colors duration-200 text-white p-2 
+      rounded block w-full text-center">
+      Log in
+    </router-link>
+
+  </div>
+
+
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
+const props = defineProps<{
+  usernameID: string;
+  usernamePlaceholder: string;
+  passwordID: string;
+  passwordPlaceholder: string;
+
+}>();
+
+const username = ref('');
+const router = useRouter();
+const password = ref('');
+
+const redirectToTank = () => {
+  if (username.value.trim() && password.value.trim()) //Ensure the input is not empty 
+  {
+    router.push('/tank/');
+  } else {
+    alert('Please enter a valid corridor ID.');
+  }
+};
+
+
+</script>
+
+<style scoped>
+
+
+</style>
