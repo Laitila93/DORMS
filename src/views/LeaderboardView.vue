21c26e51a35ce27d889707a005c87054ac1a5dc9
Emanru <emil.anbacken.rudfeldt@gmail.com>
2025-04-15 19:02:18 +0200

    create first elements in LeaderBoardView

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
 
+
+    <div class="flex flex-col min-h-screen w-screen">
+  
+      <header class=" text-white p-4 text-center">
+        <h1 class="text-2xl font-bold">Welcome to DORMS Leaderboard</h1>
+        <p class="text-lg">Here you can see who currently leads the race!</p>
+      </header>
+      
+      <main class="flex flex-col flex-1 justify-center items-center px-4">
+
+      </main>
+  
+    
+    <NavComponent :menu="menuType" />
+  
     </div>
-</template>
+  
+  </template>
+  
+  <script setup lang="ts">
+  import LoginComponent from '@/components/LoginComponent.vue';
+  import NavComponent from '@/components/NavComponent.vue';
+  import { ref } from 'vue';
+  const menuType = ref('main');
+  </script>
+  
+  <style scoped>
+  </style>
 
-<script setup lang="ts">
-
-</script>
-
-<style scoped>
-
-</style>
