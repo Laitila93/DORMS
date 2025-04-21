d31680f83babdee17225618dc4506381f9832d7c
EdvinEnglund <edvingeenglund@gmail.com>
2025-04-21 12:31:53 +0200

    render fish in tankview

--- src/views/TankView.vue
+++ src/views/TankView.vue
@@ -2,7 +2,15 @@
     <NavComponent :socket="socket" :menu="menuType" />
     Number of fish: {{ numberOfFish }}
     <div class="tank">
-      <div class="water" :style="{ height: waterLevel + '%' }"></div>
+      <div class="water" :style="{ height: waterLevel + '%' }">
+        <FishComponent
+          v-for="fish in shopData.fish"
+          :key="fish.name"
+          :fishType="fish.name"
+          :hatType="''"
+          :socket="socket"
+        />
+      </div>
     </div>
   </template>
   
@@ -10,6 +18,7 @@
   import NavComponent from '@/components/NavComponent.vue';
   import { onMounted, ref } from 'vue';
   import TankComponent from '@/components/TankComponent.vue';
+  import FishComponent from '@/components/FishComponent.vue';
   import { socket } from '@/composables/socket';
   
   const menuType = ref('tank');
@@ -19,14 +28,33 @@
   
   const waterLevel = ref(50); // Initial vattennivå
   const numberOfFish = ref(0);
-  
+  const shopData = ref<{ fish: any[] }>({ fish: [] });
+
   const updateWaterLevel = () => {
     waterLevel.value = 50 + Math.floor(Math.random() * 50); // Simulera vattennivå mellan 50 och 100
     numberOfFish.value = parseInt((waterLevel.value / 10).toString()); // Beräkna antal fiskar baserat på vattennivå
   };
   onMounted(() => {
+    const cachedData = sessionStorage.getItem('shopData');
+    if (cachedData) {
+      shopData.value = JSON.parse(cachedData);
+    } else {
+      socket.emit("getShopData", "en");
+    }
+
+    socket.on("shopData", (data) => {
+      const normalizedData = {
+        fish: data.fishes,
+        hats: data.hats,
+        specials: data.specials
+      };
+      sessionStorage.setItem('shopData', JSON.stringify(normalizedData));
+      shopData.value = normalizedData;
+    });
     setInterval(updateWaterLevel,1000);
   });
+
+
   </script>
   
   <style scoped>
