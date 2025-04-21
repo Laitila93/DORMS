29f8d1b3225f33202cd4637a5fabda24cfca558e
EdvinEnglund <edvingeenglund@gmail.com>
2025-04-21 11:37:46 +0200

    render tank in tankview

--- src/views/TankView.vue
+++ src/views/TankView.vue
@@ -1,14 +1,14 @@
 <template>
     <NavComponent :socket="socket" :menu="menuType" />
     Number of fish: {{ numberOfFish }}
-    <div class="tank-container">
-      <TankComponent :onWaterLevelChange="updateWaterLevel" />
+    <div class="tank">
+      <div class="water" :style="{ height: waterLevel + '%' }"></div>
     </div>
   </template>
   
   <script setup lang="ts">
   import NavComponent from '@/components/NavComponent.vue';
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
   import TankComponent from '@/components/TankComponent.vue';
   import { socket } from '@/composables/socket';
   
@@ -20,14 +20,31 @@
   const waterLevel = ref(50); // Initial vattennivå
   const numberOfFish = ref(0);
   
-  const updateWaterLevel = (level: number) => {
-    waterLevel.value = level;
-    numberOfFish.value = parseInt((level / 10).toString()); // Beräkna antal fiskar baserat på vattennivå
+  const updateWaterLevel = () => {
+    waterLevel.value = 50 + Math.floor(Math.random() * 50); // Simulera vattennivå mellan 50 och 100
+    numberOfFish.value = parseInt((waterLevel.value / 10).toString()); // Beräkna antal fiskar baserat på vattennivå
   };
+  onMounted(() => {
+    setInterval(updateWaterLevel,1000);
+  });
   </script>
   
   <style scoped>
   .tank-container {
     position: relative;
+  }
+  .tank {
+    width: 80vw;
+    height: 80vh;
+    border-width: 20px;
+    border-color: black;
+    border-style: solid;
+    position: relative;
+  }
+  .water {
+    width: 100%;
+    background-color: blue;
+    position: absolute;
+    bottom: 0;
   }
   </style>
