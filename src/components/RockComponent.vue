1ffbac0c864936c99802f9fbc1d7156c689ecc54
Emanru <emil.anbacken.rudfeldt@gmail.com>
2025-04-24 18:13:12 +0200

    place the rock at the bottom of tank

--- src/components/RockComponent.vue
+++ src/components/RockComponent.vue
@@ -3,7 +3,7 @@
   <div 
     class="absolute z-2 bottom-0 left-1/2 w-2/5 h-96 flex items-end justify-center overflow-hidden" 
 >   <img 
-      src="../assets/rockFormation_cut.png" 
+      src="../assets/rockFormationFinalCut.png" 
       class="w-full h-full object-contain">
   </div>
 
