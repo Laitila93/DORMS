b3964e880b3a9c23618e5820662ab965614b3d80
EdvinEnglund <edvingeenglund@gmail.com>
2025-05-07 21:26:58 +0200

    locked items in greyscale in selectors

--- src/components/FishComponent.vue
+++ src/components/FishComponent.vue
@@ -52,6 +52,10 @@
           :src="currentHat.image"
           :alt="currentHat.name"
           class="h-24 object-contain"
+          :style="{
+            filter: isHatAvailable ? 'none' : 'grayscale(100%)',
+            opacity: isHatAvailable ? 1 : 0.6
+          }"
         />
         <div v-else class="text-gray-500 flex justify-center items-center w-24 h-24 font-bold">
           No hat
@@ -90,6 +94,10 @@
           :src="currentFish.image"
           :alt="currentFish.name"
           class="h-24 object-contain"
+          :style="{
+            filter: isFishAvailable ? 'none' : 'grayscale(100%)',
+            opacity: isFishAvailable ? 1 : 0.6
+          }"
         />
         <button @click.stop="nextFish" class="bg-blue-400 hover:bg-blue-500 text-white font-bold text-2x1 px-4 py-1 rounded-full shadow transition duration-300">→</button>
       </div>
