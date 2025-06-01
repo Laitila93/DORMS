c5272f750858fbfbb37cff6c600dd9a4e61a3af0
Emanru <emil.anbacken.rudfeldt@gmail.com>
2025-06-01 20:02:34 +0200

    Fix socket event listener for XP updates in useScoreData

--- src/composables/useScoreData.ts
+++ src/composables/useScoreData.ts
@@ -35,7 +35,7 @@ export function useScoreData(socket: Socket) {
   // If any of the data is missing or outdated, emit events and update sessionStorage
   if (cachedXp === null) {
     socket.emit('getXp', dormID.value);
-    socket.off('xp').on('xp', (data) => {
+    socket.off('xp:update').on('xp:update', (data) => {
 
       xpScore.value = data;
       setSessionData('xpScore', data);
